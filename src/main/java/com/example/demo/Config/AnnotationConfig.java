c5d429d5c39ff51bfe622536aa14932843180168
ignat <krzyho.91@o2.pl>
2023-03-04 19:48:54 +0100

    popravione aop i encja cart

--- src/main/java/com/example/demo/Config/AnnotationConfig.java
+++ src/main/java/com/example/demo/Config/AnnotationConfig.java
@@ -1,9 +1,9 @@
 package com.example.demo.Config;
 
 import lombok.extern.apachecommons.CommonsLog;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,14 +16,9 @@ public class AnnotationConfig {
     public void myAnnotationMethod() {
     }
 
-    @Before("myAnnotationMethod()")
-    public void logsBefore() {
-        log.info("POST CART BEFORE");
-    }
-
-
-    @After("myAnnotationMethod()")
-    public void logsAfter() {
-        log.info("POST CART AFTER");
+    @Around("myAnnotationMethod()")
+    public Object logsBefore(ProceedingJoinPoint pjp) throws Throwable {
+        log.info("AROUND POST LOG");
+        return pjp.proceed();
     }
 }
