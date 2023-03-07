3e4deb437e13f4b0e069e889eb43a88202262297
kignatiuk <krzyho.91@o2.pl>
2023-03-07 13:37:26 +0100

    yml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableConfigServer
@@ -12,5 +13,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
     }
 }
